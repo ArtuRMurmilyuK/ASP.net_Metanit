4f4dc58904f3ef31314c93a770565f72d7ee4ac3
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2022-03-15 09:59:59 +0200

    Added html

--- HelloAppWeb/HelloAppWeb/Program.cs
+++ HelloAppWeb/HelloAppWeb/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.Run(async (context) =>
+{
+    var response = context.Response;
+    response.ContentType = "text/html; charset=utf-8";
+    await response.WriteAsync("<h2>Hello Metanit.com</h2><h3>Welcome to Asp.net</h3>");
+});
 app.Run();
